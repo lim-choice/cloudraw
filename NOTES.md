c40b1b7c57437ce15bd22be564c9eb3c08a290cf
Jihyeong <fbwlgud11@gmail.com>
2024-05-08 10:37:58 +0900

    임선택 사과해

---
+++ NOTES.md
@@ -0,0 +1,13 @@
+# Nclient
+
+## 기능정의
+
+1. `resourcemanager`로 attach/detach tag 사용
+
+2. tag를 기반으로 자원 query, ruleset은 *JMESPath*
+
+## 기능 상세
+
+1. vpc, subnet, acl, acg, vm
+
+- list, get, attach, detach
--- core.py
+++ core.py
@@ -3,43 +3,23 @@ import sys
 from typing import Union
 from argparse import ArgumentParser, _SubParsersAction
 from configparser import ConfigParser
-from models.base import NcloudCredential
-from models.dtype import NcloudServiceList
 from services.vpc import NcloudVPC
 from importlib import import_module
 from importlib.util import spec_from_file_location, module_from_spec
+from models.base import NcloudCredential
+from models.dtype import NcloudServiceList
+
 
 
 class NcloudClient:
-    def __init__(self):
+    def __init__(self, sysargv: list[str]):
         self.VERSION = "Nclient 1.0"
         self.DEBUG = True
         self.load_profile()
-        project_root = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-        
-        parser = ArgumentParser(prog="Nclient")
-        parser.add_argument('-p', '--profile', action='store', type=str, default='DEFAULT', help='Profile')
-        parser.add_argument('--debug', action='store_true', default=False, help='Enable debug mode')
-        parser.add_argument('-v', '--version', action='version', version=f"{self.VERSION}", help='Print version and exit')
-        
-        subparser = parser.add_subparsers(title="Supported Service", dest="subcommand")
-        for service in NcloudServiceList:
-            subparser.add_parser(name=service.value)
-        args = parser.parse_args()
-        
-        print(f"Subcommand = {args.subcommand}")
-        dynamic_command_value = NcloudServiceList._value2member_map_[args.subcommand].name
-        print(f"dynamic command value = {dynamic_command_value}")
-        dynamic_class_value = f"Ncloud{dynamic_command_value}"
-        print(f"dynamic class value = {dynamic_class_value}")
-        file_path = f"{project_root}/services/{args.subcommand}.py"
-        
-        
-        
-        
-        
-        
-        
+    
+    def matching_service(self):
+        pass
+
     def load_profile(self, profile_name = "DEFAULT"):
         cfg = ConfigParser()
         cfg.read(self.__load_config())
@@ -51,3 +31,6 @@ class NcloudClient:
 
     def __load_config(self):
         return os.path.join(os.path.expanduser('~'), '.ncloud', 'configure')
+
+    def __version__(self):
+        return self.VERSION
